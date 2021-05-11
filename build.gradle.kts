3cbab9454b646e9e00dfb1c5b8d5543b5c79cba2
johnGachihi <johngachihi3@gmail.com>
2021-05-11 19:13:38 +0300

    Update kotlin version to latest; 1.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.4.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.4.32"
-    kotlin("plugin.spring") version "1.4.32"
-    kotlin("plugin.jpa") version "1.4.32"
+    val kotlinVersion = "1.5.0"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
 }
 
 group = "com.example"
